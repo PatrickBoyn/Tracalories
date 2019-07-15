342528ee3ce6c0df142b5d93e7493b1eee672b20
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-14 23:05:41 -0400

    Realized the id's weren't in order.

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const ItemCtrl = (() => {
   const state = {
     items: [
       { id: 0, name: 'stake dinner', calories: 1200 },
-      { id: 2, name: 'cookie', calories: 400 },
-      { id: 3, name: 'eggs', calories: 300 }
+      { id: 1, name: 'cookie', calories: 400 },
+      { id: 2, name: 'eggs', calories: 300 }
     ],
     currentItem: null,
     totalCalories: 0
