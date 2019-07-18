39b90d92f08b79fbf0c55c6a239d638732133d5c
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-17 21:55:22 -0400

    Got the individual number, and logged it.

--- app.js
+++ app.js
@@ -148,7 +148,8 @@ const App = ((ItemCtrl, UICtrl) => {
     if (e.target.classList.contains('edit-content')) {
       const listId = e.target.parentNode.id;
       const listArray = listId.split('-');
-      console.log(listArray);
+      const id = parseInt(listArray[1]);
+      console.log(id);
     }
 
     e.preventDefault();
